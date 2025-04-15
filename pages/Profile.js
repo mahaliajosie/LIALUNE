d1f0feae2f145309780b99a7b066223162609a11
mahaliajosie <mahaliaevans101@gmail.com>
2025-04-15 10:20:13 -0400

    fixes to Profile scroll

--- pages/Profile.js
+++ pages/Profile.js
@@ -77,11 +77,12 @@ export default function Profile({ navigation }) {
             )}
         </View>
 
-        <View style={styles.sectionWrapper}>
+        <View style={styles.titleWrapper}>
             <Text style={styles.sectionTitle}>Routine</Text>
         <ScrollView 
             contentContainerStyle={styles.scrollContainer}
             showsVerticalScrollIndicator={false}
+            bounces={true}
         >
             {/* --------------------------------------------------------- */}
             {/* ---------------------- Routine Sections ---------------------- */}
@@ -155,33 +156,12 @@ export default function Profile({ navigation }) {
                 <Text style={styles.menuText}>color</Text>
                 <Ionicons name="chevron-forward" size={20} color="#CCCCCC" />
             </Pressable>
-            {/* <View style={styles.section}> 
-                <Option label="face" icon="happy-outline" />
-                <Option label="hair" icon="cut-outline" />
-                <Option label="body" icon="body-outline" />
-                <Option label="nails" icon="finger-print-outline" />
-                <View style={styles.divider} /> 
-                <Option label="favorites" icon="heart-outline" />
-                <Option label="color" icon="color-palette-outline" />
-            </View> */}
             {/* -------------------------------------------------------------- */}   
             </ScrollView>
             </View>
     </View>
   );
 }
-
-// Function that gives each category row with a left icon, text, then right arrow
-// function Option({ label, icon }) {
-//     return (
-//       <Pressable style={styles.optionRow}>
-//         <Ionicons name={icon} size={20} color="#125DAB" style={styles.optionIcon} />
-//         <Text style={styles.optionText}>{label}</Text>
-//         <Ionicons name="chevron-forward" size={20} color="#125DAB" style={{ marginLeft: 'auto' }} />
-//       </Pressable>
-//     );
-//   }  
-
 
 // UI Style for Profile
 const styles = StyleSheet.create({
@@ -194,6 +174,8 @@ const styles = StyleSheet.create({
     profileSection: {
         alignItems: 'center',   // center pfp & name
         marginBottom: 20,       // space below profile
+        borderBottomColor: '#70C1FF', // light gray line under each item
+        borderBottomWidth: 1,
     },
     profileWrapper: {
         position: 'relative',     // edit icon over the profile image
@@ -205,7 +187,7 @@ const styles = StyleSheet.create({
         overflow: 'hidden', // clip image to circle
         borderWidth: 2,
         borderColor: '#EADDCA', // Light Cream Border
-        // marginTop: 45,
+        // marginTop: 25,
       },
       editIcon: {
         position: 'absolute', // icon on top of Profile pic & anchored to the bottom-right
@@ -216,63 +198,61 @@ const styles = StyleSheet.create({
         padding: 5,
       },
   
-  nameText: {
-    fontSize: 42,
-    marginTop: 18, // space between name & profile picture
-    fontFamily: 'Italiana_400Regular',
-    color: '#70C1FF', // Lialune Blue
-  },
-  nameInput: {
-    fontSize: 42,
-    marginTop: 18,
-    fontFamily: 'Italiana_400Regular',
-    color: '#1E3D59',
-    borderBottomWidth: 1,
-    borderColor: '#EADDCA',
-  },
-  titleWrapper: {
-    paddingHorizontal: 20,
-    flex: 1,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontFamily: 'RammettoOne_400Regular',
-    color: '#0068B8', // Deep Blue for "Routine" title
-    marginBottom: 10,
-    textAlign: 'left',
-    paddingHorizontal: 16,
-  },
-  scrollContainer: {
-    flexGrow: 1,
-    justifyContent: 'flex-start',
-    paddingHorizontal: 48,         // side padding for all scroll container
-    paddingBottom: 80,             // bottom padding so last items aren't cut off
-  },
-  menuRow: {
-    flexDirection: 'row', // makes row horizontal
-    alignItems: 'center',
-    paddingVertical: 25,
-    borderBottomColor: '#EEE', // light gray line under each item
-    borderBottomWidth: 1,
-  },
-  icon: {
-    marginRight: 12, // add space between icon & label
-  },
-  menuText: {
-    flex: 1,          // take up remaining space so arrow is pushed to far right
-    fontSize: 18,
-    color: '#0068B8', // Deep Blue for each category/Option
-    fontFamily: 'Gantari_500Medium',
-  },
-  divider: {
-    // borderBottomColor: '#EADDCA', // slightly bolder line for section break
-    // borderBottomWidth: 4,
-    // marginVertical: 20,
-    height: 4,                     // thicker line
-    width: '75%',                  // shorter line
-    backgroundColor: '#EADDCA',    // light cream color
-    borderRadius: 10,              // round edges
-    alignSelf: 'center',           // center in container
-    marginVertical: 20,            // spacing above and below
-  },
+    nameText: {
+        fontSize: 42,
+        marginTop: 18, // space between name & profile picture
+        fontFamily: 'Italiana_400Regular',
+        color: '#70C1FF', // Lialune Blue
+    },
+    nameInput: {
+        fontSize: 42,
+        marginTop: 18,
+        fontFamily: 'Italiana_400Regular',
+        color: '#1E3D59',
+        borderBottomWidth: 1,
+        borderColor: '#EADDCA',
+    },
+    titleWrapper: {
+    // paddingHorizontal: 20,
+        flex: 1,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontFamily: 'RammettoOne_400Regular',
+        color: '#0068B8', // Deep Blue for "Routine" title
+        marginBottom: 10,
+        textAlign: 'left',
+        paddingHorizontal: 16,
+    },
+    scrollContainer: {
+        flexGrow: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingHorizontal: 48,         // side padding for all scroll container
+        paddingBottom: 50,             // bottom padding so last items aren't cut off
+    },
+    menuRow: {
+        flexDirection: 'row', // makes row horizontal
+        alignItems: 'center',
+        paddingVertical: 25,
+        borderBottomColor: '#EEE', // light gray line under each item
+        borderBottomWidth: 1,
+    },
+    icon: {
+        marginRight: 12, // add space between icon & label
+    },
+    menuText: {
+        flex: 1,          // take up remaining space so arrow is pushed to far right
+        fontSize: 18,
+        color: '#0068B8', // Deep Blue for each category/Option
+        fontFamily: 'Gantari_500Medium',
+    },
+    divider: {
+        height: 4,                     // thicker line
+        width: '75%',                  // shorter line
+        backgroundColor: '#EADDCA',    // light cream color
+        borderRadius: 10,              // round edges
+        alignSelf: 'center',           // center in container
+        marginVertical: 20,            // spacing above and below
+    },
 });
