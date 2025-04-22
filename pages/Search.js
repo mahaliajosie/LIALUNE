5674cb576651ed0247439bde127b6eb92e63a631
mahaliajosie <mahaliaevans101@gmail.com>
2025-04-22 14:21:47 -0400

    update Search

--- pages/Search.js
+++ pages/Search.js
@@ -6,14 +6,14 @@
 // - [ \\Displays search results matching user input\\ ]
 // ---------------------------------------------
 import React, { useState } from 'react';
-import { View, 
+import { 
+    View, 
     TextInput, 
     StyleSheet, 
     Pressable, 
     KeyboardAvoidingView, 
-    Platform } from 'react-native';
-
-// import { Ionicons } from '@expo/vector-icons';
+    Platform, 
+    StatusBar, } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faCircleChevronLeft } from '@fortawesome/free-solid-svg-icons'
 import colors from '../constants/colors';
@@ -23,46 +23,46 @@ import productData from '../data/productData';              // SAMPLE product ar
 // ////////////////////////////////////////////////////////////////////////////////////////////
 // ////////////////////////////////////////////////////////////////////////////////////////////
 // ////////////////////////////////////////////////////////////////////////////////////////////
-// import { useNavigation } from '@react-navigation/native';
 
-export default function Search({navigation}) {
-//   const navigation = useNavigation();
-
+export default function Search({ navigation }) {
   const [query, setQuery] = useState('');
 
-  // Filters Results by top 5 most relevant
+  // * Filters Results by top 5 most relevant
   const filteredResults = productData.filter((product) => {
         const combined = `${product.brand} ${product.name}`.toLowerCase()
         return combined.includes(query.toLowerCase());
-  }).slice(0, 5);
-
-//   const filteredProducts = productData.filter(product =>
-//     product.name.toLowerCase().includes(query.toLowerCase())
-//   ).slice(0, 5); // top 5 results only
+  }).slice(0, 5); // top 5 results only
 
   return (
     <KeyboardAvoidingView
         behavior={Platform.OS === 'ios' ? 'padding' : undefined}
         style={styles.container}
     >
-      {/* --------- Header with Search Bar & Back Button --------- */}
-      <View style={styles.searchContainer}>
+      {/* ------------------ H E A D E R ------------------ */}
+      <View style={styles.outerSearchContainer}>
+        {/* --------- Back Button --------- */}
         <Pressable onPress={() => navigation.goBack()} style={styles.backButton}>
-          {/* <Ionicons name="arrow-back" size={28} color={colors.primaryDeepBlue} /> */}
-          {/* <FontAwesomeIcon icon="fa-solid fa-circle-chevron-left" size={28} color={colors.primaryDeepBlue} /> */}
-          <FontAwesomeIcon icon={faCircleChevronLeft} size={28} color={colors.primaryDeepBlue} />
+          <FontAwesomeIcon icon={faCircleChevronLeft} size={32} color={colors.primaryDeepBlue} />
         </Pressable>
-        <TextInput
-          style={styles.inputSearchText} 
-          value={query}
-          onChangeText={setQuery}
-          placeholder="Find your product!"
-          placeholderTextColor={colors.lightCream}
-          autoFocus
+        {/* --------- Search Bar --------- */}
+        <View style={styles.searchContainer}>
+          <TextInput
+            style={styles.inputSearchText} 
+            value={query}
+            onChangeText={setQuery}
+            placeholder="Find your product!"
+            placeholderTextColor={colors.lightCream}
+            autoFocus
+          />
+        </View>
+        {/* --------- Status Bar --------- */}
+        <StatusBar
+            translucent
+            backgroundColor="transparent"
+            barStyle="dark-content"  // or 'light-content' depending on background
         />
       </View>
-
-      {/* --------- Condtional Results if Searching --------- */}
+      {/* --------- Search Results --------- */}
       {query !== '' && (
         <FoundProducts 
             result={filteredResults || []} 
@@ -80,21 +80,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.backgroundBlue,
-    paddingHorizontal: 16,
-    paddingTop: 80,
   },
-  searchContainer: {
+  outerSearchContainer: {
     flexDirection: 'row',
     alignItems: 'center',
+    backgroundColor: colors.lightCream,
+    paddingHorizontal: 16,
+    paddingBottom: 20,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight + 20 : 80,
+    // marginHorizontal: 16,    
+  },
+  searchContainer: {
+    flex: 1, // Search Bar fills the remaining space
+    justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: colors.slightDarkerCream,
     // margin: 20,
     borderRadius: 30,
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    height:50,
+    paddingHorizontal: 20,
+    // paddingVertical: 10,
+    height: 50,
   },
   backButton: {
-    marginRight: 10,
+    marginRight: 16,
   },
   inputSearchText: {
     flex: 1,
